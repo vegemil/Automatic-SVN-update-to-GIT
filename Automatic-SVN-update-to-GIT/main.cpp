907af38f8a660fd763c200aadbb2f9a25b4f22db
으나 :) <euna501@gmail.com>
2018-07-02 21:54:56 +0900

    한글 안깨지고 현재 리비전보다 높은 로그들 가져옴

--- Automatic-SVN-update-to-GIT/main.cpp
+++ Automatic-SVN-update-to-GIT/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 
 
-std::string repositoryPath = "D:/MOMAKaKao/trunk";
+std::string repositoryPath = " D:/MOMAKaKao/trunk";
 const int MAX_LENGTH = 300;
 
-std::string getCurrentRevision()
+std::string getCommandResult(std::string &command)
 {
 	FILE* file;
 	char output[MAX_LENGTH];
-	std::string command = "svn info --show-item last-changed-revision ";
-	command.append(repositoryPath);
 
 	file = _popen(command.c_str(), "r");
 	if (file == NULL)
@@ -26,10 +24,35 @@ std::string getCurrentRevision()
 	return output;
 }
 
+std::string getCurrentRevision()
+{
+	std::string command = "svn info --show-item last-changed-revision";
+	command.append(repositoryPath);
+
+	return getCommandResult(command);
+}
+
+void getLogList(std::string currentRevision)
+{
+	std::string command = "svn log --xml -r ";
+	command.append(currentRevision);
+ 	command.append(":HEAD ");
+ 	command.append(repositoryPath);
+
+	getCommandResult(command);
+}
+
+
 int main(void)
 {
+	std::string option = "chcp 65001";
+	getCommandResult(option);
+
 	std::string currentRevision;
 	currentRevision = getCurrentRevision();
+	currentRevision.pop_back();
+
+	getLogList(currentRevision);
 
 	return 0; 
 }
