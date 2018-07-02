624700436c2013faad08735708bdc222d1f46b2b
으나 :) <euna501@gmail.com>
2018-07-02 21:27:52 +0900

    현재 리비전 가져오기

--- Automatic-SVN-update-to-GIT/main.cpp
+++ Automatic-SVN-update-to-GIT/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+
+std::string repositoryPath = "D:/MOMAKaKao/trunk";
+const int MAX_LENGTH = 300;
+
+std::string getCurrentRevision()
+{
+	FILE* file;
+	char output[MAX_LENGTH];
+	std::string command = "svn info --show-item last-changed-revision ";
+	command.append(repositoryPath);
+
+	file = _popen(command.c_str(), "r");
+	if (file == NULL)
+	{
+		perror("erro : ");
+		exit(0);
+	}
+
+	while (fgets(output, MAX_LENGTH, file) != NULL)
+	{
+		printf("%s", output);
+	}
+
+	return output;
+}
+
 int main(void)
 {
+	std::string currentRevision;
+	currentRevision = getCurrentRevision();
+
 	return 0; 
 }
