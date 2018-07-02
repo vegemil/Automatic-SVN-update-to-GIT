bdc3bc1831856cdcafecd9cadcab7f9e9c0b9600
으나 :) <euna501@gmail.com>
2018-07-02 22:23:22 +0900

    xml 파싱 추가

--- Automatic-SVN-update-to-GIT/main.cpp
+++ Automatic-SVN-update-to-GIT/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include "tinyxml2.h"
 
 std::string repositoryPath = " D:/MOMAKaKao/trunk";
 const int MAX_LENGTH = 300;
@@ -8,6 +9,7 @@ std::string getCommandResult(std::string &command)
 {
 	FILE* file;
 	char output[MAX_LENGTH];
+	std::string xml;
 
 	file = _popen(command.c_str(), "r");
 	if (file == NULL)
@@ -19,9 +21,11 @@ std::string getCommandResult(std::string &command)
 	while (fgets(output, MAX_LENGTH, file) != NULL)
 	{
 		printf("%s", output);
+		xml.append(output);
+		xml.pop_back();
 	}
 
-	return output;
+	return xml;
 }
 
 std::string getCurrentRevision()
@@ -32,27 +36,41 @@ std::string getCurrentRevision()
 	return getCommandResult(command);
 }
 
-void getLogList(std::string currentRevision)
+void getLogList(std::string currentRevision, std::string& xml)
 {
 	std::string command = "svn log --xml -r ";
 	command.append(currentRevision);
  	command.append(":HEAD ");
  	command.append(repositoryPath);
 
-	getCommandResult(command);
+	xml = getCommandResult(command);
 }
 
+void getRevisionList(std::string& xml, std::vector<std::string>& revisionList)
+{
+	tinyxml2::XMLDocument doc;
+	doc.Parse(xml.c_str());
+
+	doc.Print();
+}
 
 int main(void)
 {
+	// 한글 셋팅
 	std::string option = "chcp 65001";
 	getCommandResult(option);
 
+	// 현재 리비전 갖고오기
 	std::string currentRevision;
 	currentRevision = getCurrentRevision();
-	currentRevision.pop_back();
 
-	getLogList(currentRevision);
+	// 로그 리스트 갖고오기
+	std::string xml;
+	getLogList(currentRevision, xml);
+
+	// 리비전 리스트 갖고 오기
+	std::vector<std::string> revisionList;
+	getRevisionList(xml, revisionList);
 
 	return 0; 
 }
